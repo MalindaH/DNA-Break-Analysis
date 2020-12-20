f94d24dcc1f012e92817e6912d3aa776dc4aac67
MalindaH <36162640+MalindaH@users.noreply.github.com>
2020-12-20 01:33:47 -0500

    MATLAB plot peaks script

--- scripts/scripts-old/chrplot_pqval.m
+++ scripts/scripts-old/chrplot_pqval.m
@@ -5,10 +5,10 @@ y=0;
 firstChr=1;
 lastChr=22;
 
-outputName='outputtest0826';
-pq='q';
-chrnum=10;
-
+outputName='outputtest1216';
+pq='p';
+chrnum=1;
+c='X';
 c=int2str(chrnum);
 
     filenameStem=strcat(outputName,'chr',c,'_',pq,'val.txt');
@@ -16,10 +16,10 @@ c=int2str(chrnum);
     hfig=figure;
     set(hfig,'Position',[0,0,2000,550]);
     %stem(STEM(:,1),STEM(:,2),'.');
-    stem(STEM(:,1),-10*log10(STEM(:,2)),'.')
-    ylim([0 50.00000])
+    stem(STEM(:,1),-log10(STEM(:,2)),'.')
+    ylim([0 20.00000])
     set(gca,'XTick',0)
     set(gca,'XTickLabel','')
     %ylabel('-10.log_1_0({\itq}-value)','fontsize',12);
-    ylabel('-10log_{10}(pval)','fontsize',12)
+    ylabel('-log_{10}(p-val)','fontsize',12)
     title(['\fontsize{13}chromosome ',c,' ',pq,'-value plot'])
